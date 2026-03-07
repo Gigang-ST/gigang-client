aa50f9a43a0439525f447b174bb65e47494c7d91
HGLEE <gusrms4650@naver.com>
2026-03-07 22:24:03 +0900

    feat: add web app manifest and apple-touch-icon for homescreen install

---
+++ app/manifest.ts
@@ -0,0 +1,27 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "기강",
+    short_name: "기강",
+    description:
+      "운동을 좋아하는 사람들이 모여 만든 스포츠 팀. 러닝, 자전거, 수영, 여행을 함께합니다.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    icons: [
+      { src: "/android-icon-36x36.png", sizes: "36x36", type: "image/png" },
+      { src: "/android-icon-48x48.png", sizes: "48x48", type: "image/png" },
+      { src: "/android-icon-72x72.png", sizes: "72x72", type: "image/png" },
+      { src: "/android-icon-96x96.png", sizes: "96x96", type: "image/png" },
+      { src: "/android-icon-144x144.png", sizes: "144x144", type: "image/png" },
+      {
+        src: "/android-icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      { src: "/ms-icon-310x310.png", sizes: "310x310", type: "image/png" },
+    ],
+  };
+}
