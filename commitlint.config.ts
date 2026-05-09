c2d796e6650a0f103a2cf930fcec002001781e8c
Evan <clcl6084@gmail.com>
2026-05-09 23:53:02 +0900

    chore: husky + commitlint + commitizen 설정 추가 (#241)

---
+++ commitlint.config.ts
@@ -0,0 +1,8 @@
+// https://www.conventionalcommits.org
+// https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional
+//
+// @commitlint/config-conventional 허용 타입:
+// build, chore, ci, docs, feat, fix, perf, refactor, revert, style, test
+export default {
+  extends: ["@commitlint/config-conventional"],
+};
